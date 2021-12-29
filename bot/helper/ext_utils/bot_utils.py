456e9ce71c1c0f89a4f3602c3208cdb9ac264edf
course9870 <96621049+course9870@users.noreply.github.com>
2021-12-29 23:50:35 +0530

    Add files via upload

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -32,7 +32,7 @@ class MirrorStatus:
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['❆', '❆❆', '❆❆❆', '❆❆❆❆', '❆❆❆❆❆', '❆❆❆❆❆❆', '❆❆❆❆❆❆❆']
+PROGRESS_INCOMPLETE = ['❆', '❆', '❆', '❆', '❆', '❆', '❆']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -109,7 +109,7 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '❆' * cFull
+    p_str = '○' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
     p_str += ' ' * (PROGRESS_MAX_SIZE - cFull)
@@ -132,7 +132,7 @@ def get_readable_message():
             INDEX += 1
             if INDEX > COUNT:
                 msg += f"╭─📂<b>Filename:</b> <code>{download.name()}</code>"
-                msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
+                msg += f"\n├─📝<b>Completed:</b> <i>{download.status()}</i>"
                 if download.status() not in [
                     MirrorStatus.STATUS_ARCHIVING,
                     MirrorStatus.STATUS_EXTRACTING,
